3b6d6ce4df7d9e96ca9a1f54c1608195531f7e1b
exiw <51192517+exiw@users.noreply.github.com>
2023-08-22 13:20:22 +0300

    Initial Commit

---
+++ SuperKahramanSwiftUI/SuperKahramanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanSwiftUIApp.swift
+//  SuperKahramanSwiftUI
+//
+//  Created by Emre Konukpay on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
